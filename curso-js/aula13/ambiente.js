5abc966ddf223d00e4a79f4d72d8afbe4c6b249e
YagoBastos <Yagobastos02102004@gmail.com>
2022-01-22 15:11:37 -0300

    novo exercicio

---
+++ curso-js/aula13/ambiente.js
@@ -0,0 +1,19 @@
+//Laços de repetição -> padão, como das outras linguagens de programação
+
+var i=1;
+//
+while(i<=3){
+    console.log(`Passo ${i}`)
+    i++;
+}
+//
+i=1;
+//
+do{
+    console.log(`${i} Passo`)
+    i++;
+}while(i<=3);
+//
+for(i=1;i<=3;i++){
+    console.log(`Passo: ${i}`)
+}
